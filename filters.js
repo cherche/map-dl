3ddaf3bacf66058b7dcf3dbac78591647f86e3f9
Ryan Nguyen <contactryannguyen@gmail.com>
2022-08-21 22:32:20 -0400

    Fixed pdftoppm wrapper tmp business

--- filters.js
+++ filters.js
@@ -1,17 +1,17 @@
 const fs = require('fs')
-const { exec } = require('child_process')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const tmp = require('tmp')
 
-function pdftoppm ({
+async function pdftoppm ({
   input,
   output,
   page,
   resolution
 }) {
-  // Using tmp/ is tough because there may be name conflicts across formulae
-  const tmpPath = `tmp/${output.split('/').join('-')}`
-  exec(`pdftoppm ${input} ${tmpPath} -png -f ${page} -singlefile -r ${resolution}`, () => {
-    fs.rename(`${tmpPath}.png`, output, err => console.log(err))
-  })
+  const tmpPath = tmp.tmpNameSync('tmp')
+  await exec(`pdftoppm ${input} ${tmpPath} -png -f ${page} -singlefile -r ${resolution}`)
+  fs.rename(`${tmpPath}.png`, output, err => console.log(err))
 }
 
 module.exports = { pdftoppm }

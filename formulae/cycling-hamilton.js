38b6a412b392147a81b49fff1eaf50b0b5414a23
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2022-09-22 11:55:40 -0400

    Update scraper cycling-hamilton

--- formulae/cycling-hamilton.js
+++ formulae/cycling-hamilton.js
@@ -3,15 +3,37 @@ const filters = require('../filters')
 module.exports = {
   name: 'cycling-hamilton',
   shortName: 'Hamilton',
-  site: 'https://www.hamilton.ca/streets-transportation/biking-cyclists/',
+  site: 'https://www.hamilton.ca/home-neighbourhood/getting-around/biking-cyclists',
   type: 'webscrape',
-  scrapeUrl: 'https://www.hamilton.ca/streets-transportation/biking-cyclists/cycling-routes-maps',
+  scrapeUrl: 'https://www.hamilton.ca/home-neighbourhood/getting-around/biking-cyclists/cycling-routes-maps',
   downloads: [
+    {
+      id: 'Rural',
+      getUrl () {
+        const anchors = document.querySelector('.content-wrapper').querySelectorAll('a')
+        return anchors[0].href
+      },
+      extension: 'pdf',
+      outputs: [
+        {
+          id: 'default',
+          omitId: true,
+          generate: (input, output) => {
+            filters.pdftoppm({
+              input,
+              output,
+              page: 1,
+              resolution: 150
+            })
+          }
+        }
+      ]
+    },
     {
       id: 'Urban',
       getUrl () {
-        const mapList = document.querySelector('.pane-content .pane-content ul')
-        return mapList.querySelectorAll('li a')[0].href
+        const anchors = document.querySelector('.content-wrapper').querySelectorAll('a')
+        return anchors[1].href
       },
       extension: 'pdf',
       outputs: [
@@ -29,27 +51,5 @@ module.exports = {
         }
       ]
     },
-    {
-      id: 'Rural',
-      getUrl () {
-        const mapList = document.querySelector('.pane-content .pane-content ul')
-        return mapList.querySelectorAll('li a')[1].href
-      },
-      extension: 'pdf',
-      outputs: [
-        {
-          id: 'default',
-          omitId: true,
-          generate: (input, output) => {
-            filters.pdftoppm({
-              input,
-              output,
-              page: 1,
-              resolution: 150
-            })
-          }
-        }
-      ]
-    }
   ]
 }
